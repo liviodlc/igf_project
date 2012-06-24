2b44ee86e995c73e9e2e408634760be23438f60b
Livio De La Cruz <livio@interguild.org>
2012-06-24 15:12:21 -0700

    Second Commit

---
+++ src/igf/project/Main.java
@@ -0,0 +1,13 @@
+package igf.project;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!");
+	}
+
+}
