d1d057bd6cb2c5cf5c0b36f59b1c0e6fe5c6bdb2
Livio De La Cruz <livio@interguild.org>
2012-06-24 15:52:52 -0700

    test.

--- src/igf/project/Main.java
+++ src/igf/project/Main.java
@@ -7,7 +7,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Livio's Edit");
 	}
 
 }
