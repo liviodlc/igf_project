99cb62b7be839a0f406793be40525051c1088b98
Zuoming Shi <accountistaken@gmail.com>
2012-06-24 17:14:57 -0700

    Zuoming's First edit in the experimental branch.

--- src/igf/project/Main.java
+++ src/igf/project/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Experimental Feature: beep");
+		System.out.println("Zuoming's Experimental Feature: Summon Meteors");
 	}
 
 }
