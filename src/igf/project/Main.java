7412608acbb3b07e1f92515605f6b5c3b6dad71e
Livio De La Cruz <livio@interguild.org>
2012-06-26 20:22:14 -0700

    Removed a useless comment.

--- src/igf/project/Main.java
+++ src/igf/project/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Livio's Nth Edit");
 		System.out.println("Zuoming's First Edit [EDIT: Livio was here]");
